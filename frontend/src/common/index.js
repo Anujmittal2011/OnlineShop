ed92d8d83f8e6e6b11d5c703604997c26cca26e4
Anujmittal2011 <150452014+Anujmittal2011@users.noreply.github.com>
2025-05-03 17:03:11 +0530

    Update index.js

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,84 +1,97 @@
 // const backendDomin = "http://localhost:8080"
 const backendDomin = "https://onlineshop-aq8x.onrender.com"
 
+
 const SummaryApi = {
-    signUP : {
-        url : `${backendDomin}/api/signup`,
-        method : "post"
-    },
-    signIn : {
-        url : `${backendDomin}/api/signin`,
-        method : "post"
-    },
-    current_user : {
-        url : `${backendDomin}/api/user-details`,
-        method : "get"
-    },
-    logout_user : {
-        url : `${backendDomin}/api/userLogout`,
-        method : 'get'
-    },
-    allUser : {
-        url : `${backendDomin}/api/all-user`,
-        method : 'get'
-    },
-    updateUser : {
-        url : `${backendDomin}/api/update-user`,
-        method : "post"
-    },
-    uploadProduct : {
-        url : `${backendDomin}/api/upload-product`,
-        method : 'post'
-    },
-    allProduct : {
-        url : `${backendDomin}/api/get-product`,
-        method : 'get'
-    },
-    updateProduct : {
-        url : `${backendDomin}/api/update-product`,
-        method  : 'post'
-    },
-    categoryProduct : {
-        url : `${backendDomin}/api/get-categoryProduct`,
-        method : 'get'
-    },
-    categoryWiseProduct : {
-        url : `${backendDomin}/api/category-product`,
-        method : 'post'
-    },
-    productDetails : {
-        url : `${backendDomin}/api/product-details`,
-        method : 'post'
-    },
-    addToCartProduct : {
-        url : `${backendDomin}/api/addtocart`,
-        method : 'post'
-    },
-    addToCartProductCount : {
-        url : `${backendDomin}/api/countAddToCartProduct`,
-        method : 'get'
-    },
-    addToCartProductView : {
-        url : `${backendDomin}/api/view-card-product`,
-        method : 'get'
-    },
-    updateCartProduct : {
-        url : `${backendDomin}/api/update-cart-product`,
-        method : 'post'
-    },
-    deleteCartProduct : {
-        url : `${backendDomin}/api/delete-cart-product`,
-        method : 'post'
-    },
-    searchProduct : {
-        url : `${backendDomin}/api/search`,
-        method : 'get'
-    },
-    filterProduct : {
-        url : `${backendDomin}/api/filter-product`,
-        method : 'post'
-    }
-}
+  signUP: {
+    url: `${backendDomin}/api/signup`,
+    method: "post",
+    withCredentials: true
+  },
+  signIn: {
+    url: `${backendDomin}/api/signin`,
+    method: "post",
+    withCredentials: true
+  },
+  current_user: {
+    url: `${backendDomin}/api/user-details`,
+    method: "get",
+    withCredentials: true
+  },
+  logout_user: {
+    url: `${backendDomin}/api/userLogout`,
+    method: "get",
+    withCredentials: true
+  },
+  allUser: {
+    url: `${backendDomin}/api/all-user`,
+    method: "get",
+    withCredentials: true
+  },
+  updateUser: {
+    url: `${backendDomin}/api/update-user`,
+    method: "post",
+    withCredentials: true
+  },
+  uploadProduct: {
+    url: `${backendDomin}/api/upload-product`,
+    method: "post",
+    withCredentials: true
+  },
+  allProduct: {
+    url: `${backendDomin}/api/get-product`,
+    method: "get"
+  },
+  updateProduct: {
+    url: `${backendDomin}/api/update-product`,
+    method: "post",
+    withCredentials: true
+  },
+  categoryProduct: {
+    url: `${backendDomin}/api/get-categoryProduct`,
+    method: "get"
+  },
+  categoryWiseProduct: {
+    url: `${backendDomin}/api/category-product`,
+    method: "post"
+  },
+  productDetails: {
+    url: `${backendDomin}/api/product-details`,
+    method: "post"
+  },
+  addToCartProduct: {
+    url: `${backendDomin}/api/addtocart`,
+    method: "post",
+    withCredentials: true
+  },
+  addToCartProductCount: {
+    url: `${backendDomin}/api/countAddToCartProduct`,
+    method: "get",
+    withCredentials: true
+  },
+  addToCartProductView: {
+    url: `${backendDomin}/api/view-card-product`,
+    method: "get",
+    withCredentials: true
+  },
+  updateCartProduct: {
+    url: `${backendDomin}/api/update-cart-product`,
+    method: "post",
+    withCredentials: true
+  },
+  deleteCartProduct: {
+    url: `${backendDomin}/api/delete-cart-product`,
+    method: "post",
+    withCredentials: true
+  },
+  searchProduct: {
+    url: `${backendDomin}/api/search`,
+    method: "get"
+  },
+  filterProduct: {
+    url: `${backendDomin}/api/filter-product`,
+    method: "post"
+  }
+};
 
-
-export default SummaryApi
+export default SummaryApi;
